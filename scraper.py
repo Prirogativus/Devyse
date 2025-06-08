88fe8dd61d0249a573658fb35ad5d50080980e19
Prirogativus <prirogativus@gmail.com>
2025-06-08 16:55:34 +0200

    Fully working scraper
    Fixed the bugs interfering program execution

--- scraper.py
+++ scraper.py
@@ -8,8 +8,8 @@ regular_html_page ="https://www.olx.pl/elektronika/komputery/laptopy/krakow/?pag
 
 
 def get_pagination_numbers(html: str):
-    html_page = requests.get(html).text
-    soup = BeautifulSoup(html_page, 'lxml')
+    
+    soup = get_html_page(html)
     pagination = soup.find_all('a', class_ = "css-b6tdh7")
 
     print("Getting pagination numbers...")
@@ -25,41 +25,69 @@ def get_pagination_numbers(html: str):
     return page_numbers
 
 def get_listings(html: str):
-    html_page = requests.get(html).text
-    soup = BeautifulSoup(html_page, 'lxml')
 
+    #Finds all listings on the given page 
+    #Returns a list of dictionaries with laptop information and description
+
+    soup = get_html_page(html)
     devices = []
 
     print("Starting to scrape listings...")
-    listings = soup.find_all('a', class_ = "css-1tqlkj0")
+    listings = soup.find_all('div', class_ = "css-l9drzq")
 
     for listing in listings:
+
         print("Working on listing...")
-        link = 'https://www.olx.pl' + listing.get('href')
-        #description_page = requests.get(link).text
-        #soup = BeautifulSoup(description_page, 'lxml')
-        #description = soup.find('div', class_ = 'css-19duwlz').text
-        for child in listing.contents:
-            print(child) 
-            print('\n')
+
+        link = 'https://www.olx.pl' + listing.find('a', class_ =  'css-1tqlkj0').get('href')
+
         laptop = {
             'title': listing.find('h4', class_ = 'css-1g61gc2').text,
-            'price': listing.parent.parent.find('p', class_ = 'css-uj7mm0').text.strip(),
-            'status': listing.parent.parent.find('span', class_ = 'css-iudov9').text,
-            'location': listing.parent.parent.find('p', class_ = 'css-vbz67q').text,
+            'price': listing.find('p', class_ = 'css-uj7mm0').text.strip(),
+            'status': listing.find('span', class_ = 'css-iudov9').text,
+            'location': listing.find('p', class_ = 'css-vbz67q').text,
             'link': link,
-            #'descrption': description
+            'description': get_description(link)
         }
+
         devices.append(laptop)
-        print("Adding laptop: ", laptop['title'])
+        
+        print(f"Laptop {laptop['title']} data: \n" 
+              f"Price: {laptop['price']}, \n"
+              f"Status: {laptop['status']}, \n"
+              f"location: {laptop['location']}, \n"
+              f"link: {laptop['link']}, \n"
+              f"description: {laptop['description'][:50]}...\n")  # Print first 50 characters of description
+        
     return devices
+
+def get_description(url: str):
+
+    # Gets the desctiption of the Laptop from the given page
+
+    soup = get_html_page(url)
+    description = soup.find('div', class_ = 'css-19duwlz').text
+    return description
+
+def get_html_page(url: str):
+
+    # Function to get HTML page from a given URL
+
+    print("Getting HTML page...")
+
+    html_page = requests.get(url).text
+    soup = BeautifulSoup(html_page, 'lxml')
+
+    print("HTML page retrieved.")
+
+    return soup
     
 
 def main():
     laptops = []
     for number in range(max(get_pagination_numbers(first_html_page))):
         page_number = number + 1
-        print ("Working on page: ", page_number)
+        print (f"Page: {page_number}\n")
         laptops = laptops + get_listings(regular_html_page.replace('page=2', f'page={page_number}')) 
     print("Finished scraping laptops.")
 
